6d4eb12fe0c85fcddf596e420b1e5f38b5b4a40e
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-16 12:43:16 -0500

    Los componentes deden estar declarados en el archivo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomComponent } from './components/hom/hom.component';
+import { ErrorComponent } from './components/error/error.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
