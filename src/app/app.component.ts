55eb1c43e507fce66fe3c8272b59b4602addd7c9
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-16 11:24:29 -0500

    Iniciando con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'videos-angular-symfony4';
+  title = 'Aplicación de videos favoritos';
 }
