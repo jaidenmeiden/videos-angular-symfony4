fda870d07a763581cb2a1c76a83ff391e30972b3
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-18 10:34:14 -0500

    Persistir usuario identificado en el frontend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../models/user';
 import {UserService} from '../../services/user.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,9 @@ export class LoginComponent implements OnInit {
   public status: string;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute,
   ) {
     this.pageTitle = 'Login';
     this.user = new User(1, '', '', '', '', 'ROLE_USER', '', '', '');
@@ -40,6 +43,11 @@ export class LoginComponent implements OnInit {
 
                 console.log(this.identity);
                 console.log(this.token);
+
+                localStorage.setItem('token', this.token);
+                localStorage.setItem('identity', JSON.stringify(this.identity));
+
+                this._router.navigate(['/inicio']);
               } else {
                 this.status = 'error';
               }
