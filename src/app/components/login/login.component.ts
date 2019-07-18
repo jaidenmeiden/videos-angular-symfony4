1091aba6c813b1bda6a043db0fe7bb968ad235f8
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-18 09:58:36 -0500

    Login de usuarios

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
   public pageTitle: string;
+  public user: User;
+  public status: string;
 
-  constructor() {
+  constructor(
+    private _userService: UserService
+  ) {
     this.pageTitle = 'Login';
+    this.user = new User(1, '', '', '', '', 'ROLE_USER', '', '', '');
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form) {
+    console.log(this.user);
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -32,4 +32,13 @@ export class UserService {
     return this._http.post(this.url + 'user/create', params, {headers:headers});
   }
 
+  login(user):Observable<any> {
+    let json = JSON.stringify(user);
+    let params = 'json='+json;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this._http.post(this.url + 'user/create', params, {headers:headers});
+  }
+
 }
